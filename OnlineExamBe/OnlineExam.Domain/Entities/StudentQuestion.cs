8098c73b152f6dd1f045d00b9d27a66460fd1438
huyhuyqazwsx <huyhuyqaz121233@gmail.com>
2025-10-24 15:15:08 +0700

    update

---
+++ OnlineExamBe/OnlineExam.Domain/Entities/StudentQuestion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineExam.Domain.Entities
+{
+    public class StudentQuestion
+    {
+        public int StudentId { get; set; }
+        public int QuestionExamId { get; set; }
+        public required string Answer { get; set; }
+        public float? Result { get; set; }
+
+        public User Student { get; set; }
+        public QuestionExam QuestionExam { get; set; }
+    }
+
+}
