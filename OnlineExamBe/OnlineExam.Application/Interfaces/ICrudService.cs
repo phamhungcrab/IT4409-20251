65c7091d2b5c827a28cfff76ca3defcfd400b150
huyhuyqazwsx <huyhuyqaz121233@gmail.com>
2025-10-25 11:47:44 +0700

    base for CRUD

---
+++ OnlineExamBe/OnlineExam.Application/Interfaces/ICrudService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineExam.Application.Interfaces
+{
+    public interface ICrudService<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+        Task CreateAsync(T entity);
+        Task<bool> UpdateAsync(T entity);
+        Task<bool> DeleteAsync(int id);
+    }
+}
---
+++ OnlineExamBe/OnlineExam.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineExam.Domain.Interfaces
+{
+    public interface IRepository<T> where T: class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+        Task AddAsync(T entity);
+        void DeleteAsync(T entity);
+        void UpdateAsync(T entity);
+        Task SaveChangesAsync();
+    }
+}
