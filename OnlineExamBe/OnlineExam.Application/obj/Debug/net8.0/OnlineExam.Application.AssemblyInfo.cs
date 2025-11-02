adfa7f0c10328d37ca800c905418dfda7c8f24d7
Le Huong <Dongnguyen210300@gmail.com>
2025-11-02 22:36:15 +0700

    .

--- OnlineExamBe/OnlineExam.Application/obj/Debug/net8.0/OnlineExam.Application.AssemblyInfo.cs
+++ OnlineExamBe/OnlineExam.Application/obj/Debug/net8.0/OnlineExam.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineExam.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecce883350dc0a515b85a3213858ea071fb51210")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07d9dbaeaa80ccaa5829aea570f926a65b6cc40b")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineExam.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineExam.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
