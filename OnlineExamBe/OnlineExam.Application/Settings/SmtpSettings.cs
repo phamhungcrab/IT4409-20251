543686cbf8b8b2dd55fdf15fd63fdd51b20c3e68
Le Huong <Dongnguyen210300@gmail.com>
2025-11-11 14:58:12 +0700

    .

--- OnlineExamBe/OnlineExam.Application/Settings/SmtpSettings.cs
+++ OnlineExamBe/OnlineExam.Application/Settings/SmtpSettings.cs
@@ -9,8 +9,8 @@ namespace OnlineExam.Application.Settings
     public class SmtpSettings
     {
         public string Host { get; set; } = string.Empty;
-        public int Port { get; set; }
-        public bool EnableSsl { get; set; }
+        public int Port { get; set; } 
+        public bool EnableSsl { get; set; } 
         public string Username { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
     }
